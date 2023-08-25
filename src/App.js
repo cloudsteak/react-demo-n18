d179aa74fa9d89439aae2be3ec34ac94602e3911
Tibor Kiss <support@cloudsteak.com>
2023-08-25 14:23:57 +0200

    Remove link

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://mentorklub.hu"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          mentor Klub tanfolyamok
-        </a>
       </header>
     </div>
   );

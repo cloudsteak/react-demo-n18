ed0e4b365a52cbf26d9e144f553cf79d5d38fcec
Tibor Kiss <support@cloudsteak.com>
2024-01-11 13:54:48 +0100

    Fix?

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Tanulj jó dolgokat! :-)
+          Tanulj jó dolgokat!
         </a>
       </header>
     </div>
